e32c6e0a4738e60bbe365942a23e0d68aac52c24
Teddy Franceschi <ttf6@duke.edu>
2017-11-27 19:42:04 -0500

    adds uiobjects I created

---
+++ FirePage/CustomUIObjects/FIreTextField.swift
@@ -0,0 +1,52 @@
+//
+//  FIreTextField.swift
+//  FirePage
+//
+//  Created by Theodore Franceschi on 11/21/17.
+//  Copyright © 2017 Theodore Franceschi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class FireTextField: UITextField {
+    
+    // Provides left padding for images
+    override func rightViewRect(forBounds bounds: CGRect) -> CGRect {
+        var textRect = super.rightViewRect(forBounds: bounds)
+        textRect.origin.x += leftPadding
+        return textRect
+    }
+    
+    @IBInspectable var rightImage: UIImage? {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var leftPadding: CGFloat = 0
+    
+    @IBInspectable var color: UIColor = UIColor.white {
+        didSet {
+            updateView()
+        }
+    }
+    
+    func updateView() {
+        if let image = rightImage {
+            rightViewMode = UITextFieldViewMode.always
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+            imageView.image = image
+            // Note: In order for your image to use the tint color, you have to select the image in the Assets.xcassets and change the "Render As" property to "Template Image".
+            imageView.tintColor = color
+            rightView = imageView
+        } else {
+            rightViewMode = UITextFieldViewMode.never
+            rightView = nil
+        }
+        
+        // Placeholder text color
+        attributedPlaceholder = NSAttributedString(string: placeholder != nil ?  placeholder! : "", attributes:[NSAttributedStringKey.foregroundColor: color])
+    }
+}
